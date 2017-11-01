8f54b9f72e355922470a6dd8101f077cbe8839b1
Garmeil <mayo.cody@gmail.com>
2017-11-01 15:36:35 -0500

    assignment complete

---
+++ DevMtn/precourse/Projects/Basecamp Beginner Code/Cody's Website/Practice Worksheet.js
@@ -0,0 +1,12 @@
+var carCompanies = [
+    "Toyota",
+    "Subaru",
+    "Ford",
+    "Chevy",
+    "GMC",
+    "Porsche"
+];
+var cody = "Cody";
+cody; 
+console.log(carCompanies[2]<=carCompanies[4])
+console.log(carCompanies[2])
---
+++ DevMtn/precourse/Projects/Basecamp Beginner Code/Practice Worksheet.js
@@ -0,0 +1,16 @@
+var carCompanies = [
+    "Toyota",
+    "Subaru",
+    "Ford",
+    "Chevy",
+    "GMC",
+    "Porsche"
+];
+var cody = "Cody";
+cody; 
+console.log(carCompanies[2]<=carCompanies[4])
+console.log(carCompanies[2])
+
+
+
+
